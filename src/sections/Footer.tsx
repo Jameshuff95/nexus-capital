1d341e00b3a58985993cdf58662f286a24e4adbf
Jameshuff95 <126839048+Jameshuff95@users.noreply.github.com>
2024-08-31 07:50:31 -0400

    completed footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,7 +1,62 @@
+import NexusLight from "../assets/logos/kobodrop_logo_bright.png";
+import StoreLinks from "../common/StoreLinks";
+import Twitter from "../assets/logos/icons8-twitterx.svg";
+import Facebook from "../assets/logos/icons8-facebook.svg";
+import Linkedin from "../assets/logos/icons8-linkedin.svg";
+
 function Footer() {
   return (
-    <section id="footer">
-      <h1>Footer</h1>
+    <section id="footer" className="bg-gray-800">
+      <div className="flex max-w-7xl flex-col px-8 py-12 text-gray-300 lg:px-12 xl:m-auto">
+        <div className="flex flex-col justify-between gap-12 md:flex-row md:gap-0">
+          <div>
+            <img
+              src={NexusLight}
+              alt="Light nexus logo"
+              className="mb-4 w-36"
+            />
+            <p>Start spending the smart way</p>
+          </div>
+          <div className="flex gap-10">
+            <div className="flex flex-col gap-2">
+              <p className="text-gray-400">Product</p>
+              <a href="#">Overview</a>
+              <a href="#">Features</a>
+              <a href="#">Solutions</a>
+              <a href="#">Contact</a>
+              <a href="#">Releases</a>
+            </div>
+            <div className="flex flex-col gap-2">
+              <p className="text-gray-400">Resources</p>
+              <a href="#">Blog</a>
+              <a href="#">Newsletter</a>
+              <a href="#">Events</a>
+              <a href="#">Help Center</a>
+              <a href="#">Support</a>
+            </div>
+          </div>
+          <div className="w-fit">
+            <h2>Get the app</h2>
+            <StoreLinks />
+          </div>
+        </div>
+        <div className="mt-14 flex justify-between border-t-2 border-t-gray-700 pt-10">
+          <p className="w-16[ch] text-gray-400 md:w-full">
+            &copy; 2024 Nexus. All rights reserved.
+          </p>
+          <div className="flex gap-6">
+            <a href="https://www.twitter.com/" target="_blank">
+              <img src={Twitter} alt="twitter logo" />
+            </a>
+            <a href="https://www.facebook.com/" target="_blank">
+              <img src={Facebook} alt="facebook logo" />
+            </a>
+            <a href="https://www.linkedin.com/" target="_blank">
+              <img src={Linkedin} alt="linkedin logo" />
+            </a>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
